33996a2ae31d701d129ff765a3b4b92e5d856348
Martin Meudre <martin.meudre@telecom-sudparis.eu>
2026-01-14 15:44:49 +0100

    Ajout des mesh collider sur les obstacles du parcours et implémentation des contrôles claviers classiques du personnage.

---
+++ My project/Assets/PlayerMov.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+    void Update()
+    {
+        MyInput();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+}
